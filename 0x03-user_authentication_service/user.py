9839b7a4112393cb92e12d8bce10d07fca8450cd
Prosper <prosperorjiogo1999@gmail.com>
2024-07-19 05:16:05 +0100

    user auth

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,17 +1,26 @@
-from sqlalchemy.ext. declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/env python3
+"""
+Declare a SQLAlchemy model named 'User' corresponding to a
+database table named "users"
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """
+    Definition of class User
+    """
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
-
-    def __repr__(self):
-        return f'<User {self.id}, {self.email}, {self.hashed_password}, {self.session_id}, {self.reset_token}>'
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
